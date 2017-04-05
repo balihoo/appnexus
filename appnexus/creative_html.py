24368b2ee0dd0f64c9642dfc0fa77b5eb171f8fa
Jeff Flitton <jflitton@balihoo.com>
2017-04-05 16:49:59 -0600

    Adding CreativeHtml.deactivate

--- appnexus/creative_html.py
+++ appnexus/creative_html.py
@@ -3,3 +3,22 @@ class CreativeHtml(SubService):
     service_name = 'creative-html'
     collection_name = 'creatives'
 
+    def deactivate(self):
+        """ sets the creative state to inactive """
+        if self.data.get('id'):
+            payload = {
+                self.service_name: {
+                    'state': 'inactive',
+                    'status': {
+                        'user_ready': False
+                    }
+                }
+            }
+            res = self._client.put('{service_name}?id={id}&advertiser_id={advertiser_id}'.format(
+                service_name=self.service_name,
+                id=self.data['id'],
+                advertiser_id=self.data['advertiser_id'],
+            ), payload)
+            self.data.update(res[self.service_name])
+
+        return True
